0684305fb298153c6dbd70a97d997a11f7ee9091
andreasfmalde <andreas.malde@lyse.net>
2025-06-11 12:54:23 +0200

    feature to edit waypoint information and return an updated geojson route

--- src/geoJSON/index.js
+++ src/geoJSON/index.js
@@ -1,5 +1,5 @@
-import { curveWaypointLeg } from "../route.js";
-import { convertGeoJsonWaypointToRouteWaypoint } from "./parser.js";
+import { curveWaypointLeg, RouteToGeoJSON } from "../route.js";
+import { convertGeoJsonWaypointToRouteWaypoint, parseGeoJsonToJS } from "./parser.js";
 import { distance,point } from "@turf/turf";
 
 export function editCurveSegment(gWP1,gWP2,gWP3,gLeg12,gLeg23,newRadius){
@@ -37,4 +37,36 @@ export function editCurveSegment(gWP1,gWP2,gWP3,gLeg12,gLeg23,newRadius){
 
     return [gWP1,wp2.toGeoJSON(),gWP3,l12,l23];
 
+}
+
+
+export function EditRouteWaypoint(geojson, waypointID, updateParams ){
+    const { 
+        coordinates, radius, reference, name,
+        fixed, externalReferenceID, routeWaypointLeg, extensions  } = updateParams;
+
+    if ([coordinates, radius, reference, name, fixed, externalReferenceID,
+         routeWaypointLeg, extensions ]
+         .every(param => param === undefined || param === null)) {
+            
+        throw new Error("No parameters provided for update");
+    }
+
+    const [legs, waypoints, actionpoints] = parseGeoJsonToJS(geojson);
+
+    const waypoint = waypoints.find(wp => wp.getId() === waypointID);
+    if(!waypoint) {
+        throw new Error(`No waypoint found with ID: ${waypointID}`);
+    }
+
+    if(coordinates) waypoint.setCoordinates(coordinates);
+    if(radius) waypoint.setRadius(radius);
+    if(reference) waypoint.setReference(reference);
+    if(name) waypoint.setRouteWaypointName(name);
+    if(fixed !== undefined) waypoint.setRouteWaypointFixed(fixed);
+    if(externalReferenceID) waypoint.setRouteWaypointExternalReferenceID(externalReferenceID);
+    if(routeWaypointLeg) waypoint.setRouteWaypointLeg(routeWaypointLeg);
+    if(extensions) waypoint.setRouteWaypointExtensions(extensions);
+
+    return RouteToGeoJSON(legs, waypoints, actionpoints);
 }
