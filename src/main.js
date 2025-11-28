f1696352651b2938a9dc28c11bba4b47eb3c3c70
Sigrid Angell <sigridkjosavikangell@ecc.no>
2025-11-28 15:17:12 +0100

    Formatting

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import { S421ToGeoJSON } from './S421/index.js';
 import { createLayers, createCorridorLayers } from './layers.js';
 import { RTZtoGeoJSON } from './RTZ/index.js';
 import { EditRouteWaypoint, EditRouteWaypointNewRoute } from './geoJSON/index.js';
-import { RebuildRouteGeoJSON } from "./geoJSON/rebuild.js";
+import { RebuildRouteGeoJSON } from "./geoJSON/rebuild.js";
 
-export {
-  S421ToGeoJSON,
-  RTZtoGeoJSON,
-  createLayers,
-  createCorridorLayers,
-  EditRouteWaypoint,
-  EditRouteWaypointNewRoute,
-  RebuildRouteGeoJSON,
+export { 
+    S421ToGeoJSON,
+    RTZtoGeoJSON,
+    createLayers, 
+    createCorridorLayers, 
+    EditRouteWaypoint,
+    EditRouteWaypointNewRoute,
+    RebuildRouteGeoJSON
 };
